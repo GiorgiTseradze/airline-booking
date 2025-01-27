b0585d8b4cbdf2d0a0a6e4eaad0ce5d907a04738
georgetseradze <georgetseradze@gmail.com>
2025-01-27 23:23:36 +0400

    feat: implement validations and change minor styles

--- src/components/AirlineForm.tsx
+++ src/components/AirlineForm.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from "react";
 import { FlightDestination } from "@/types/FlightDestination";
-import Bounded from "./Bounded";
 import clsx from "clsx";
 import { TripTypeSelector } from "./FormComponents/TripTypeSelector";
 import { DropdownSelector } from "./FormComponents/DropdownSelector";
 import { DateSelector } from "./FormComponents/DateSelector";
 import { submitForm } from "@/app/actions/form-actions";
 import { SubmitButton } from "@/components/ui/SubmitButton";
+import Bounded from "@/components/Bounded";
+import { ValidationMessage } from "./ui/ValidationMessage";
 
 interface AirlineFormProps {
 	destinations: FlightDestination[];
@@ -17,7 +18,7 @@ interface AirlineFormProps {
 export const AirlineForm = ({ destinations }: AirlineFormProps) => {
 	const [loading, setLoading] = useState(false);
 	const [response, setResponse] = useState<string | null>(null);
-	const [tripType, setTripType] = useState("one-way");
+	const [tripType, setTripType] = useState("roundtrip");
 	const [origin, setOrigin] = useState<FlightDestination | null>(null);
 	const [destination, setDestination] = useState<FlightDestination | null>(
 		null
@@ -25,7 +26,16 @@ export const AirlineForm = ({ destinations }: AirlineFormProps) => {
 	const [departureDate, setDepartureDate] = useState<Date | undefined>();
 	const [returnDate, setReturnDate] = useState<Date | undefined>();
 
-	const handleSubmit = async () => {
+	const validations = {
+		isSameCity: !!(origin && destination && origin.code === destination.code),
+		isMissingRequiredFields:
+			!origin ||
+			!destination ||
+			!departureDate ||
+			(tripType === "roundtrip" && !returnDate),
+	};
+
+	async function handleSubmit() {
 		try {
 			setLoading(true);
 
@@ -50,61 +60,66 @@ export const AirlineForm = ({ destinations }: AirlineFormProps) => {
 		} finally {
 			setLoading(false);
 		}
-	};
+	}
 
 	return (
 		<Bounded>
-			<div className="flex flex-col gap-10">
-				<h1 className="font-body font-bold text-xl text-center">
-					Welcome to Digido Airlines!
-				</h1>
+			<div className="relative lg:mt-24 flex flex-col items-center lg:items-start gap-6 lg:gap-10">
+				<div className="relative w-60 lg:w-auto">
+					<div className="flex flex-col gap-4 lg:gap-14 lg:flex-row">
+						<DropdownSelector
+							label="Origin"
+							paramKey="origin"
+							selectedOrigin={origin}
+							onChange={setOrigin}
+							options={destinations}
+						/>
 
-				<DropdownSelector
-					label="Origin"
-					paramKey="origin"
-					selectedOrigin={origin}
-					onChange={setOrigin}
-					options={destinations}
-				/>
-
-				<DropdownSelector
-					label="Destination"
-					paramKey="destination"
-					selectedOrigin={destination}
-					onChange={setDestination}
-					options={destinations}
-				/>
-
-				<TripTypeSelector selectedType={tripType} onChange={setTripType} />
-
-				<DateSelector
-					label="Departure Date"
-					paramKey="departureDate"
-					selectedDate={departureDate}
-					onChange={setDepartureDate}
-					availableDays={destinations[0]?.availableWeekdays || []}
-				/>
-
-				<div
-					className={clsx(
-						"transition-all ease-in-out duration-500 overflow-hidden",
-						tripType === "round-trip"
-							? "max-h-40 opacity-100"
-							: "max-h-0 opacity-0"
-					)}
-				>
-					<DateSelector
-						label="Return Date"
-						paramKey="returnDate"
-						selectedDate={returnDate}
-						onChange={setReturnDate}
-						availableDays={destinations[1]?.availableWeekdays || []}
+						<DropdownSelector
+							label="Destination"
+							paramKey="destination"
+							selectedOrigin={destination}
+							onChange={setDestination}
+							options={destinations}
+						/>
+					</div>
+					<ValidationMessage
+						isInvalid={validations.isSameCity}
+						message="Origin and destination cannot be the same."
 					/>
 				</div>
 
-				<SubmitButton onClick={handleSubmit} loading={loading}>
-					Submit
-				</SubmitButton>
+				<div className="flex flex-col gap-4 lg:gap-14 lg:flex-row">
+					<DateSelector
+						label="From"
+						paramKey="departureDate"
+						selectedDate={departureDate}
+						onChange={setDepartureDate}
+						availableDays={origin?.availableWeekdays || []}
+						disabled={!origin}
+					/>
+
+					<DateSelector
+						label="To"
+						paramKey="returnDate"
+						selectedDate={returnDate}
+						onChange={setReturnDate}
+						availableDays={destination?.availableWeekdays || []}
+						minDate={departureDate}
+						disabled={tripType !== "roundtrip" || !destination}
+					/>
+				</div>
+
+				<TripTypeSelector selectedType={tripType} onChange={setTripType} />
+
+				<SubmitButton
+					onClick={handleSubmit}
+					loading={loading}
+					disabled={
+						validations.isSameCity || validations.isMissingRequiredFields
+					}
+					message="Book Flight"
+				/>
 
 				{response && <p className="text-center mt-4">{response}</p>}
 			</div>
